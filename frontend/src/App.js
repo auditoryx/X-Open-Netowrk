23d61645cc25611b7fc782207e68198c78fc9da9
auditoryx <zenji@auditoryx.com>
2025-03-07 16:59:08 +0900

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import ServiceForm from "./pages/ServiceForm";
+import ServiceList from "./pages/ServiceList";
 
 function App() {
-    return <h1>Welcome to AuditoryX Network</h1>;
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">View Services</Link></li>
+          <li><Link to="/list-service">List a Service</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<ServiceList />} />
+        <Route path="/list-service" element={<ServiceForm />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
