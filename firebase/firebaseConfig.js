ce5436c3960e959161cc5b35829e5ac71e41ad0f
auditoryx <zenji@auditoryx.com>
2025-06-05 19:28:04 +0900

    refactor firebase config to use env vars

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAUHHus8UIz1KPrQMLIc4MSZrDoHzejyPA",
-  authDomain: "auditory-x-open-network.firebaseapp.com",
-  projectId: "auditory-x-open-network",
-  storageBucket: "auditory-x-open-network.firebasestorage.app",
-  messagingSenderId: "827240797874",
-  appId: "1:827240797874:web:28e35367b510a4a34c1bab",
-  measurementId: "G-T4JEJCW28T"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
