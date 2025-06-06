e5d06ab2795e0bdb8073c8b29b72032e6b67adfb
auditoryx <zenji@auditoryx.com>
2025-06-06 13:25:03 +0900

    Validate sendgrid env vars before sending mail

--- src/lib/notifications/sendEmailNotification.ts
+++ src/lib/notifications/sendEmailNotification.ts
@@ -1,6 +1,21 @@
 import sgMail from '@sendgrid/mail'
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
+const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY
+const SENDGRID_FROM_EMAIL = process.env.SENDGRID_FROM_EMAIL
+
+if (!SENDGRID_API_KEY) {
+  const msg = 'SENDGRID_API_KEY is not defined'
+  console.error(msg)
+  throw new Error(msg)
+}
+
+if (!SENDGRID_FROM_EMAIL) {
+  const msg = 'SENDGRID_FROM_EMAIL is not defined'
+  console.error(msg)
+  throw new Error(msg)
+}
+
+sgMail.setApiKey(SENDGRID_API_KEY)
 
 export async function sendEmailNotification({
   to,
@@ -16,7 +31,7 @@ export async function sendEmailNotification({
   try {
     await sgMail.send({
       to,
-      from: process.env.SENDGRID_FROM_EMAIL as string,
+      from: SENDGRID_FROM_EMAIL,
       subject,
       text,
       html: html || `<p>${text}</p>`
