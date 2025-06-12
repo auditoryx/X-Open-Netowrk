99e04a6abba5802f85946e1044d232e811403360
auditoryx <zenji@auditoryx.com>
2025-06-12 06:45:10 +0000

    test(explore): smoke test for FilterPanel UI

---
+++ src/__tests__/filterPanel.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import FilterPanel from '../components/explore/FilterPanel';
+
+test('renders genre chips & BPM inputs', () => {
+  const filters = { role: '', location: '', service: '', genres: [], sort:'rating' };
+  render(<FilterPanel filters={filters as any} setFilters={() => {}} />);
+  expect(screen.getByLabelText(/Genres/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/Min BPM/i)).toBeInTheDocument();
+});
