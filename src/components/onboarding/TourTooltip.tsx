ec7a97e3a3044475efff60c7a9c14379586bb206
auditoryx <zenji@auditoryx.com>
2025-06-22 15:49:41 +0900

    feat: add global TourTooltip component

---
+++ src/components/onboarding/TourTooltip.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
+
+export default function TourTooltip() {
+  return <Tooltip id="tour-tooltip" place="bottom" />;
+}
